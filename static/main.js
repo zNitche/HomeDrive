ee2b1c3899a173fe6a97a7144156c7c1c024d801
TheZodiaCC <53448802+TheZodiaCC@users.noreply.github.com>
2020-12-28 20:47:10 +0100

    tweaked upload button

--- static/main.js
+++ static/main.js
@@ -2,4 +2,5 @@ function getFileName(myFile){
    var file = myFile.files[0];
 
    document.getElementById("file_name").innerHTML = file.name;
+   document.getElementById("upload_button").style.display = "block";
 }
