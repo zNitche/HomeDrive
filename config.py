1544448d39d3206d32d5e9769fa0fb25d125ea4a
TheZodiaCC <53448802+TheZodiaCC@users.noreply.github.com>
2021-01-07 00:00:19 +0100

    changed tmp and files location for easier mounting

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
     CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
     MAX_UPLOAD_SIZE = 2000  # max uploaded file size in MB
     MAX_SHARED_FILES_SIZE = 50000000000 # max size of files stored in system in bytes
-    FILES_LOCATION = f"{CURRENT_DIR}/files/"
-    TMP_LOCATION = f"{CURRENT_DIR}/tmp/"
-    PRIVATE_FILES_LOCATION = f"{CURRENT_DIR}/private/"
+    FILES_LOCATION = f"{CURRENT_DIR}/storage/files/"
+    TMP_LOCATION = f"{CURRENT_DIR}/storage/tmp/"
+    PRIVATE_FILES_LOCATION = f"{CURRENT_DIR}/storage/private/"
     APP_PORT = 8080
