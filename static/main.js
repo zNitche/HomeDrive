c51d20a3502e7504e7d7e9202b5ff4525e0366ae
TheZodiaCC <53448802+TheZodiaCC@users.noreply.github.com>
2021-01-16 00:20:53 +0100

    added delete confirmation modal

--- static/main.js
+++ static/main.js
@@ -4,3 +4,13 @@ function getFileName(myFile){
    document.getElementById("file_name").innerHTML = file.name;
    document.getElementById("upload_button").style.display = "block";
 }
+
+function showDeleteModal(value){
+    document.getElementById('delete-modal').style.display='flex'
+    document.getElementById('delete-modal-button').value = value;
+    document.getElementById('delete-file-name').innerText = "Do you want to delete " + value + " ?";
+}
+
+function closeDeleteModal(){
+    document.getElementById('delete-modal').style.display='none';
+}
