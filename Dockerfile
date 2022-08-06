5dde0812848b4859581b5d40a7cbc7e18de2fde7
TheZodiaCC <53448802+TheZodiaCC@users.noreply.github.com>
2022-08-06 18:17:25 +0200

    added basic mysql setup

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,7 @@ COPY . /HomeDrive
 
 WORKDIR /HomeDrive
 
+RUN apt update && apt -y install default-libmysqlclient-dev
 RUN pip3 install -r requirements.txt
 
 CMD gunicorn -c gunicorn.conf.py app:app --preload
--- app.py
+++ app.py
@@ -1,7 +1,15 @@
+import dotenv
+import os
+
+
+dotenv.load_dotenv(os.path.join(os.path.dirname(os.path.realpath(__file__)), ".env"))
+
+
 from __init__ import create_app
 
 
 app = create_app()
+
 
 if __name__ == "__main__":
     APP_PORT = app.config["APP_PORT"]
